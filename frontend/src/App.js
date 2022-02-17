17c2134c5822517adeabe1329b1199935421ae2f
AnilKOC <adewry.ak@gmail.com>
2022-02-17 15:58:40 +0300

    Front-End & Serving & Fetching Data From Back-End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import { Container } from 'react-bootstrap'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <div >
+    <Router >
       <Header/>
-        <main className='py-5'>
+        <main className='py-3'>
           <Container>
-          <h1>Welcome</h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
           </Container>
         </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react'
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import axios from 'axios'
+
+function ProductScreen() {
+    const match = useParams();
+    const [product, setProduct] = useState([])
+
+    useEffect (() => {
+      
+      async function fetchProduct() {
+        const { data } = await axios.get('/api/products/'+match.id)
+        setProduct(data)
+      }
+  
+      fetchProduct()
+  
+    }, [])
+
+    return (
+    <div>
+        <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+        <Row>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price: ${product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Description: {product.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price:</Col>
+                                <Col>
+                                    <strong>${product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Stock:</Col>
+                                <Col>
+                                    {product.countInStock > 0 ? product.countInStock + ' Left' : 'Out of Stock'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+        {product.name}
+    </div>
+    );
+  }
+
+export default ProductScreen
